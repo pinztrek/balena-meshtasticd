d09cd207319a5f0430116ed0f4384ffdb552ba7d
Alan Barrow <68825469+pinztrek@users.noreply.github.com>
2025-05-06 17:00:56 -0400

    setup config.yaml and device

--- meshtasticd/etc/meshtasticd/run_md.sh
+++ meshtasticd/etc/meshtasticd/run_md.sh
@@ -8,6 +8,23 @@ if [ ! "$DELAY" ];then
 	DELAY=5
 fi
 
+# install a default config.yaml if needed
+cd /etc/meshtasticd
+if [ ! -f config.yaml && -f config-dist.yaml ]; then
+	echo "Setup default config.yaml"
+	cp config-dist.yaml config.yaml
+ fi
+
+ if [ "MESHTOAD" ]; then
+ 	rm config.d/*
+ 	cp available.d/lora-usb-meshtoad-e22.yaml config.d
+  else
+  	# assume waveshare
+   	rm config.d/*
+ 	cp available.d/lora-waveshare-sxxx.yaml config.d
+  fi
+  	
+
 # check for spi
 lsmod 2>/dev/null | grep -i spi
 ls -l "/dev/spi*" 2>/dev/null
