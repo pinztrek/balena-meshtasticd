0b311659f2de92cf765aac78bb7343e350d46baf
Alan Barrow <68825469+pinztrek@users.noreply.github.com>
2025-05-06 11:56:51 -0400

    Create run_md.sh

---
+++ meshtasticd/etc/meshtasticd/run_md.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$DEBUG" ];then
+	DELAY=120
+fi
+
+if [ ! "$DELAY" ];then
+	DELAY=5
+fi
+
+# check for spi
+lsmod | grep -i spi
+ls -l /dev/spi
+
+# run the daemon
+meshtasticd
+echo "meshtasticd exited"
+sleep $DELAY
